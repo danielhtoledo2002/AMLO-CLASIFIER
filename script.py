70118cd94eeaf8860827d4a40c07364251a49a1b
Daniel <0243179@up.edu.mx>
2023-10-21 01:14:19 -0600

    regex spanish

--- script.py
+++ script.py
@@ -1,6 +1,26 @@
-import pandas as pd
+# file to experiment
+import re
 import nltk
+import numpy as np
+import pandas as pd
+import spacy
 from nltk.corpus import stopwords
+from spacy.lang.es.examples import sentences
 
+nlp = spacy.load("es_core_news_sm")
 df = pd.read_csv("amlo.csv")
 spanish_stopwords = stopwords.words("spanish")
+
+# code to delete information that doesn't have enought words
+# df = df[df["Texto"].str.split().str.len() >= 15 ]
+# df.to_csv("amlo.csv", index=False)
+
+# experiments
+def text_processing(text):
+    text = text.lower()
+    text = re.sub(r"(@[A-Za-z0-9_]+)|([^0-9A-Za-zÁáÉéÍíÓóÚúÜüÑñ \t])|(\w+:\/\/\S+)", "", text)
+    return text
+
+df["Texto"] = df["Texto"].apply(text_processing)
+
+print(df.head())
